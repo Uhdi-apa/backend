6c8e2087a5a92d76832cf3dd89e26798dfc94b50
kick_sim <tlacksgml97@gmail.com>
2025-05-03 14:05:38 +0900

    init commit

---
+++ src/main/java/com/example/uhdi_apa/UhdiApaApplication.java
@@ -0,0 +1,13 @@
+package com.example.uhdi_apa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UhdiApaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UhdiApaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/uhdi_apa/UhdiApaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uhdi_apa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UhdiApaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
